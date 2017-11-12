57d64ffb7f7eefd4c2a987beae0e49bcf0718487
NakCheonJung <ncjung1000@gmail.com>
2017-11-12 15:18:44 +0900

    Initial Commit

---
+++ SocialSearcherSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialSearcherSwift
+//
+//  Created by NakCheon Jung on 12/11/2017.
+//  Copyright © 2017 ncjung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
